6af08f048d1fb2c7f8bc6f7bcc80b5ceb572cda6
Alexander Belopashentsev <61732514+belopash@users.noreply.github.com>
2023-06-15 14:15:19 +0500

    arrowsquid (#11)

--- src/processor.ts
+++ src/processor.ts
@@ -1,139 +1,50 @@
-import {In} from 'typeorm'
-import {
-    assertNotNull,
-    BatchHandlerContext,
-    BatchProcessorItem,
-    EvmBatchProcessor,
-    EvmBlock,
-} from '@subsquid/evm-processor'
-import {Store, TypeormDatabase} from '@subsquid/typeorm-store'
 import {lookupArchive} from '@subsquid/archive-registry'
+import {
+    BlockHeader,
+    DataHandlerContext,
+    EvmBatchProcessor,
+    EvmBatchProcessorFields,
+    Log as _Log,
+    Transaction as _Transaction,
+} from '@subsquid/evm-processor'
+import {Store} from '@subsquid/typeorm-store'
 import * as erc20 from './abi/erc20'
-import {Account, Token, Transfer} from './model'
 
 // replace with a ERC20 contract address
-const CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-const CHAIN_NODE = process.env.CHAIN_NODE || 'https://rpc.ankr.com/eth'
+export const CONTRACT_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
 
-const processor = new EvmBatchProcessor()
+export const processor = new EvmBatchProcessor()
     .setDataSource({
+        // uncomment and set RPC_ENDPOONT to enable contract state queries.
+        // Both https and wss endpoints are supported.
+        // chain: process.env.RPC_ENDPOINT,
+
+        // Change the Archive endpoints for run the squid
+        // against the other EVM networks
+        // For a full list of supported networks and config options
+        // see https://docs.subsquid.io/develop-a-squid/evm-processor/configuration/
+
         archive: lookupArchive('eth-mainnet'),
-        chain: CHAIN_NODE,
+        chain: 'https://rpc.ankr.com/eth',
     })
-    .addLog(CONTRACT_ADDRESS, {
-        filter: [[erc20.events.Transfer.topic]],
-        data: {
-            evmLog: {
-                topics: true,
-                data: true,
-            },
-            transaction: {
-                hash: true,
-            },
+    .setFinalityConfirmation(10)
+    .setFields({
+        log: {
+            topics: true,
+            data: true,
+        },
+        transaction: {
+            hash: true,
         },
     })
-
-type Item = BatchProcessorItem<typeof processor>
-type Ctx = BatchHandlerContext<Store, Item>
-
-processor.run(new TypeormDatabase(), async (ctx) => {
-    let transfersData: TransferEventData[] = []
-
-    for (let {header: block, items} of ctx.blocks) {
-        for (let item of items) {
-            if (item.kind !== 'evmLog') continue
-            if (item.evmLog.topics[0] !== erc20.events.Transfer.topic) continue
-            let event = erc20.events.Transfer.decode(item.evmLog)
-            transfersData.push({
-                id: item.evmLog.id,
-                blockNumber: block.height,
-                timestamp: new Date(block.timestamp),
-                txHash: item.transaction.hash,
-                from: event.from.toLowerCase(),
-                to: event.to.toLowerCase(),
-                amount: event.value.toBigInt(),
-            })
-        }
-    }
-
-    await saveTransfers(ctx, ctx.blocks[ctx.blocks.length - 1].header, transfersData)
-})
-
-async function saveTransfers(ctx: Ctx, block: EvmBlock, transfersData: TransferEventData[]) {
-    let accountIds = new Set<string>()
-    for (let t of transfersData) {
-        accountIds.add(t.from)
-        accountIds.add(t.to)
-    }
-
-    let accounts = await ctx.store
-        .findBy(Account, {id: In([...accountIds])})
-        .then((q) => new Map(q.map((i) => [i.id, i])))
-
-    let transfers: Transfer[] = []
-
-    let token = await ctx.store.get(Token, CONTRACT_ADDRESS)
-    if (token == null) {
-        token = await fetchToken(ctx, block, CONTRACT_ADDRESS)
-        await ctx.store.insert(token)
-    }
-
-    for (let t of transfersData) {
-        let {id, blockNumber, timestamp, txHash, amount} = t
-
-        let from = getAccount(accounts, t.from)
-        let to = getAccount(accounts, t.to)
-
-        transfers.push(
-            new Transfer({
-                id,
-                blockNumber,
-                timestamp,
-                txHash,
-                from,
-                to,
-                amount,
-            })
-        )
-    }
-
-    await ctx.store.upsert(Array.from(accounts.values()))
-    await ctx.store.insert(transfers)
-}
-
-async function fetchToken(ctx: Ctx, block: EvmBlock, address: string) {
-    let contract = new erc20.Contract(ctx, block, address)
-
-    let name = await contract.name()
-    let symbol = await contract.symbol()
-    let totalSupply = await contract.totalSupply().then((r) => r.toBigInt())
-    let decimals = await contract.decimals()
-
-    return new Token({
-        id: address,
-        name,
-        symbol,
-        totalSupply,
-        decimals,
+    .addLog({
+        address: [CONTRACT_ADDRESS],
+        topic0: [erc20.events.Transfer.topic],
+        transaction: true,
     })
-}
 
-interface TransferEventData {
-    id: string
-    blockNumber: number
-    timestamp: Date
-    txHash: string
-    from: string
-    to: string
-    amount: bigint
-}
-
-function getAccount(m: Map<string, Account>, id: string): Account {
-    let acc = m.get(id)
-    if (acc == null) {
-        acc = new Account()
-        acc.id = id
-        m.set(id, acc)
-    }
-    return acc
-}
+export type Fields = EvmBatchProcessorFields<typeof processor>
+export type Context = DataHandlerContext<Store, Fields>
+export type Block = BlockHeader<Fields>
+export type Log = _Log<Fields>
+export type Transaction = _Transaction<Fields>
